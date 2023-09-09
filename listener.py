d68b20ab08134200c66de8f357197b560d302916
sdehury34 <somashekhar.dehury@gmail.com>
2023-09-09 17:05:22 +0530

    Minor changes

--- listener.py
+++ listener.py
@@ -1,12 +1,12 @@
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore, storage
 import time
 from manager import *
 from main import model
 import asyncio
 
 firCredentials = credentials.Certificate('serviceAccountKey.json')
-firApp = firebase_admin.initialize_app(firCredentials)
+firApp = firebase_admin.initialize_app(firCredentials, {'storageBucket' : 'gs://accenture-578fc.appspot.com/lost_user_image'})
 
 # Get access to Firestore
 db = firestore.client()
@@ -14,9 +14,11 @@ print('Connection initialized')
 lost_col_ref = db.collection('lostChild')
 found_col_ref = db.collection('foundChild')
 matched_col_ref = db.collection('matchedChildren')
+bucket = storage.bucket()
 
 
 def on_snapshot(doc_snapshot, changes, read_time):
+    print("-----Downloading recent uploads-----")
     asyncio.run(get_imgs())
     for doc in doc_snapshot:
         doc_dict = doc.to_dict()
@@ -25,27 +27,35 @@ def on_snapshot(doc_snapshot, changes, read_time):
         img_downloader(name, image_url, True)
         matched = model(name)
         if not matched:
-            print("No match found")
+            print("-----No match found-----")
         else:
-            print(name + 'looks similar to' + matched)
+            print('----- ' + name + ' looks similar to ' + matched + ' -----')
             lostd = lost_col_ref.document(matched).get()
             lost_dict = lostd.to_dict()
             foundd = found_col_ref.document(name).get()
             found_dict = foundd.to_dict()
-            new = found_dict.update(lost_dict)
+            new = {
+                'childName': lost_dict['childName'],
+                'childAge': lost_dict['childAge'],
+                'parentName': lost_dict['parentName'],
+                'parentsContact': lost_dict['parentsContact'],
+                'description': found_dict['foundChildDescription'],
+                'imgUrl': found_dict['imgUrl'],
+                'lostDate': lost_dict['lostDate']
+            }
             matched_col_ref.add(new)
             lost_col_ref.document(matched).delete()
-            lfile = 'Training_images' + matched + '.jpg'
-            img_deleter(lfile)
+            lost_file = 'Training_images/' + matched + '.jpg'
+            img_deleter(lost_file)
         found_col_ref.document(name).delete()
-        ffile = 'Found_images/' + name + '.jpg'
-        img_deleter(ffile)
+        found_file = 'Found_images/' + name + '.jpg'
+        img_deleter(found_file)
+
 
 doc_watch = found_col_ref.on_snapshot(on_snapshot)
 
 
 async def get_imgs():
-    print("Downloading recent uploads")
     docs = lost_col_ref.get()
     for doc in docs:
         doc_dict = doc.to_dict()
@@ -55,4 +65,5 @@ async def get_imgs():
 
 # Keep the app running
 while True:
-    time.sleep(1)
+    time.sleep(60)
+    print("Waiting for changes")
--- manager.py
+++ manager.py
@@ -4,6 +4,13 @@ import os
 lost_dir = 'Training_images'
 found_dir = 'Found_images'
 
+if not os.path.exists(lost_dir):
+    print("Folder created")
+    os.mkdir(lost_dir)
+
+if not os.path.exists(found_dir):
+    os.mkdir(found_dir)
+
 
 def img_downloader(name, url, found):
     filename = name + '.jpg'
@@ -11,12 +18,6 @@ def img_downloader(name, url, found):
         filepath = found_dir + '/' + filename
     else:
         filepath = lost_dir + '/' + filename
-
-    if not os.path.exists(lost_dir):
-        os.mkdir(lost_dir)
-
-    if not os.path.exists(found_dir):
-        os.mkdir(found_dir)
 
     if not os.path.isfile(filepath):
         urllib.request.urlretrieve(url, filepath)
